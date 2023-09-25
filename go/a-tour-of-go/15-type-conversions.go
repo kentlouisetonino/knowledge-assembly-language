95d25cbfbde00f137a8d03c8a078017dc8b3f2c4
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-09-25 21:00:07 +0800

    type conversion

---
+++ go/a-tour-of-go/15-type-conversions.go
@@ -0,0 +1,39 @@
+/*
+  Type Conversions
+    - The expression T(v) converts the value v to the type T.
+*/
+
+package main
+
+import "fmt"
+
+func numericConversion() {
+  var i int = 42
+  var f float64 = float64(i)
+  var u uint = uint(f)
+
+  fmt.Println("***** Numeric Conversion *****")
+  fmt.Println("i: ", i)
+  fmt.Println("f: ", f)
+  fmt.Println("u: ", u)
+  fmt.Println("\n")
+}
+
+func shortNumericConversion() {
+  i := 100
+  f := float64(i)
+  u := uint(f)
+
+  fmt.Println("***** Short Numeric Conversion *****")
+  fmt.Println("i: ", i)
+  fmt.Println("f: ", f)
+  fmt.Println("u: ", u)
+  fmt.Println("\n")
+
+}
+
+func main() {
+  numericConversion()
+  shortNumericConversion()
+}
+
