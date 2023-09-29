9899a0c5b81cb059067450785b832ba075246175
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-09-29 20:40:59 +0800

    constant

--- go/a-tour-of-go/01-packages-variables-functions/17-constant.go
+++ go/a-tour-of-go/01-packages-variables-functions/17-constant.go
@@ -10,9 +10,14 @@ package main
 import "fmt"
 
 const PI = 3.14
+const TEST = "TEST"
+const ONE = 1
+
 
 func main() {
   fmt.Println("***** Constants *****")
   fmt.Printf("PI: %T, %v \n", PI, PI)
+  fmt.Printf("TEST: %T, %v \n", TEST, TEST)
+  fmt.Printf("ONE: %T, %v \n", ONE, ONE)
 }
 
