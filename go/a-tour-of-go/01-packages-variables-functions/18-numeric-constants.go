08b8676bcdc3e63d50e1ee0c9b96e99a0713529c
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-09-29 21:26:56 +0800

    a little bit learning of types

--- go/a-tour-of-go/01-packages-variables-functions/18-numeric-constants.go
+++ go/a-tour-of-go/01-packages-variables-functions/18-numeric-constants.go
@@ -7,3 +7,40 @@
     - An "int" can store at maximum a 64-bit integer, and sometimes less.
 */
 
+package main
+
+import "fmt"
+import "math/bits"
+
+
+const (
+  // Shifting 1 bit left 100 places.
+  // The binary number that is 1 followed by 100 zeroes.
+  Big = 1 << 62
+
+  // Shifting 63 bits right 61 places.
+  // Preceded by 61 zeroes.
+  Small = Big >> 61
+)
+
+// The fmt package allows us to print anything on the screen.
+// The bits package allows to perform bitwise operations to unsigned integer values.
+func getBits(number uint) int {
+   // getting the binary representation of the above chosen value
+   fmt.Printf("Binary representation of %d is: %b\n", number, number)
+
+   // initializing a variable to store the results
+   var result int
+
+   // getting the number of bits in the result variable
+   result = bits.Len(number)
+
+   // returning back the number of bits
+   return result
+}
+
+func main() {
+  fmt.Println(getBits(Big));
+  fmt.Println(getBits(Small))
+}
+
