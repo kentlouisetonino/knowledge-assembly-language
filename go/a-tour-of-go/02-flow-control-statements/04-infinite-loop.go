3c761a851ba07ba79583ddeb00dc9b43b7c1120b
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2023-09-30 07:17:52 +0800

    infinite loop

---
+++ go/a-tour-of-go/02-flow-control-statements/04-infinite-loop.go
@@ -0,0 +1,15 @@
+/*
+  Note:
+    - If you omit the loop condition it loops forever.
+    - 
+*/
+
+package main
+import "fmt"
+
+func main() {
+  for {
+    fmt.Println("Infinite Loop ...")
+  }
+}
+
